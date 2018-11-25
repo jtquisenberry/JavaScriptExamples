8c7f34f80fa737a4e18dc5ac5be3ac764ef02958
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2018-11-24 23:50:58 -0800

    20181124

--- Interview_Cake/arrays/reverse_array_inplace.js
+++ Interview_Cake/arrays/reverse_array_inplace.js
@@ -1,1 +1,68 @@
-// a
+function reverse(arrayOfChars) {
+
+    // Reverse the input array of characters in place
+    var leftIndex = 0;
+    var rightIndex = arrayOfChars.length -1;
+    
+    while (leftIndex < rightIndex)
+    {
+      var temp = arrayOfChars[leftIndex];
+      arrayOfChars[leftIndex] = arrayOfChars[rightIndex];
+      arrayOfChars[rightIndex] = temp
+      
+      leftIndex += 1;
+      rightIndex -= 1;
+      
+    }
+    
+    return;
+  
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // Tests
+  
+  let desc = 'empty string';
+  let input = ''.split('');
+  reverse(input);
+  let actual = input.join('');
+  let expected = '';
+  assertEqual(actual, expected, desc);
+  
+  desc = 'single character string';
+  input = 'A'.split('');
+  reverse(input);
+  actual = input.join('');
+  expected = 'A';
+  assertEqual(actual, expected, desc);
+  
+  desc = 'longer string';
+  input = 'ABCDE'.split('');
+  reverse(input);
+  actual = input.join('');
+  expected = 'EDCBA';
+  assertEqual(actual, expected, desc);
+  
+  function assertEqual(a, b, desc) {
+    if (a === b) {
+      console.log(`${desc} ... PASS`);
+    } else {
+      console.log(`${desc} ... FAIL: ${a} != ${b}`);
+    }
+  }
