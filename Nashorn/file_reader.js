f137b583db1398b8e8c93e598564fde0dc4cf439
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2018-11-27 22:42:28 -0800

    Deleted Nashorn

--- Nashorn/file_reader.js
+++
@@ -1,6 +0,0 @@
-var bufferedReader = new java.io.BufferedReader(new java.io.FileReader("C:Temp\\json_set.txt"));
-var line;
-while ((line = bufferedReader.readLine()) != null) 
-{
-    print(line);
-}
